b513720640a07960f0a8a8f9f7785c4bb0919576
Tsakiridis Sotiris <sotiris@serres.gr>
2019-03-24 09:31:29 +0200

    Complete feature list

--- android/app/src/main/java/gr/artibet/vgames/FragmentFeatures.java
+++ android/app/src/main/java/gr/artibet/vgames/FragmentFeatures.java
@@ -1,18 +1,123 @@
 package gr.artibet.vgames;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import gr.artibet.vgames.adapters.FeatureAdapter;
+import gr.artibet.vgames.api.FeatureAPI;
+import gr.artibet.vgames.models.Feature;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class FragmentFeatures extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_features, container, false);
+    private MainActivity mainActivity;
+    private ImageView refreshButton;
+
+    private RecyclerView recyclerView;
+    private FeatureAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private List<Feature> featureList = new ArrayList<>();
+
+    public FragmentFeatures() {
+        // Required empty public constructor
     }
+
+    public static FragmentFeatures newInstance() {
+        FragmentFeatures fragment = new FragmentFeatures();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hold a reference to main activity
+        mainActivity = (MainActivity)getActivity();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_features, container, false);
+
+        // Set refresh button click listener
+        refreshButton = view.findViewById(R.id.refreshFeatureListButton);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchFeatures();
+            }
+        });
+
+        // Initialize recycler view
+        recyclerView = view.findViewById(R.id.featureRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FeatureAdapter(featureList);
+        recyclerView.setAdapter(adapter);
+        fetchFeatures();
+
+        return view;
+    }
+
+
+    // Fetch genres from API
+    private void fetchFeatures() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getResources().getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        FeatureAPI featureAPI = retrofit.create(FeatureAPI.class);
+
+        Call<List<Feature>> call = featureAPI.getFeatures();
+
+        call.enqueue(new Callback<List<Feature>>() {
+            @Override
+            public void onResponse(Call<List<Feature>> call, Response<List<Feature>> response) {
+                if (!response.isSuccessful()) {
+
+                    // Toast failure message
+                    mainActivity.showErrorToast(getString(R.string.feature_fetch_error));
+                    return;
+                }
+
+                // Change adapters data
+                featureList = response.body();
+                adapter.setFeatureList(featureList);
+            }
+
+            @Override
+            public void onFailure(Call<List<Feature>> call, Throwable t) {
+
+                // Toast failure message
+                mainActivity.showErrorToast(getString(R.string.feature_fetch_error));
+            }
+        });
+    }
+
+
+
 }
---
+++ android/app/src/main/java/gr/artibet/vgames/adapters/FeatureAdapter.java
@@ -0,0 +1,62 @@
+package gr.artibet.vgames.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import gr.artibet.vgames.R;
+import gr.artibet.vgames.models.Feature;
+
+public class FeatureAdapter extends RecyclerView.Adapter<FeatureAdapter.FeatureViewHolder> {
+
+    private List<Feature> featureList;
+
+    // Adapter's view holder
+    public static class FeatureViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewDesc;
+
+        public FeatureViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewDesc = itemView.findViewById(R.id.tvFeatureDesc);
+        }
+    }
+
+    // Constructor
+    public FeatureAdapter(List<Feature> featureList){
+
+        this.featureList = featureList;
+    }
+
+    public void setFeatureList(List<Feature> featureList) {
+        this.featureList = featureList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public FeatureViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feature_item, viewGroup, false);
+        FeatureViewHolder viewHolder = new FeatureViewHolder(v);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeatureViewHolder featureViewHolder, int i) {
+        Feature feature = featureList.get(i);
+        featureViewHolder.textViewDesc.setText(feature.getDesc());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return featureList.size();
+    }
+}
