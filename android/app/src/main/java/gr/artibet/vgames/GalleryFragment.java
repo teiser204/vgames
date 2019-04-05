f798b3764dfff9b51bfddc893ae9c2e3e546bd8c
Tsakiridis Sotiris <sotiris@serres.gr>
2019-04-05 12:02:47 +0300

    Implement popup image view on game image gallery

--- android/app/src/main/java/gr/artibet/vgames/GalleryFragment.java
+++ android/app/src/main/java/gr/artibet/vgames/GalleryFragment.java
@@ -1,13 +1,27 @@
 package gr.artibet.vgames;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,6 +78,26 @@ public class GalleryFragment extends Fragment {
             @Override
             public void onItemClick(int position) {
                 String image = mImageList.get(position);
+
+                Dialog dlg = new Dialog(getActivity());
+                dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dlg.setContentView(R.layout.dialog_image);
+
+                ImageView iv = dlg.findViewById(R.id.dialog_imageview);
+
+                Glide.with(getActivity())
+                        .load(image)
+                        .placeholder(R.drawable.ic_image_placeholder)
+                        .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                        .into(iv);
+
+                dlg.show();
+
+
+//                Glide.with(getActivity())
+//                        .load(image)
+//                        .into(iv);
+
 
                 // Open image preview
                 //Intent intent = new Intent(getActivity(), GameActivity.class);
