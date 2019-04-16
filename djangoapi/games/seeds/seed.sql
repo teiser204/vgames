878a09a978f7e0aee29602974c801d2752ad44e6
Tsakiridis Sotiris <sotiris@serres.gr>
2019-04-16 12:57:35 +0300

    Create seed data for lookup tables

---
+++ djangoapi/games/seeds/seed.sql
@@ -0,0 +1,64 @@
+# company
+insert into company (`desc`, `created_at`, `updated_at`) values ('Nintendo', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Valve Corporation', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Rockstar Games', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Electronic Arts', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Activision Blizzard', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Sony Computer Entertainment', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Ubisoft', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Sega Games Co. Ltd', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('BioWare', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Microsoft Corporation', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Epic Games', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('id Software', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Lucas Arts', '2019-04-15', '2019-04-16');
+insert into company (`desc`, `created_at`, `updated_at`) values ('Sierra', '2019-04-15', '2019-04-16');
+
+# feature
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Single-player', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Multi-player', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Co-op', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Achievements', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Leaderboards', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Controller support', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('In development', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Cloud saves', '2019-04-15', '2019-04-16');
+insert into feature (`desc`, `created_at`, `updated_at`) values ('Overlay', '2019-04-15', '2019-04-16');
+
+# genre
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Role-playing', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Simulation', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Indie', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Racing', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Sports', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Action', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Strategy', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Shooter', '2019-04-15', '2019-04-16');
+insert into genre (`desc`, `created_at`, `updated_at`) values ('Adventure', '2019-04-15', '2019-04-16');
+
+# platform
+insert into platform (`desc`, `created_at`, `updated_at`) values ('Windows', '2019-04-15', '2019-04-16');
+insert into platform (`desc`, `created_at`, `updated_at`) values ('OSX', '2019-04-15', '2019-04-16');
+insert into platform (`desc`, `created_at`, `updated_at`) values ('Linux', '2019-04-15', '2019-04-16');
+
+# language
+insert into language (`desc`, `created_at`, `updated_at`) values ('English', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Deutsch', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('français', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('español', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('italiano', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Português do Brasil', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Português', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('русский', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('polski', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('日本語', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('český', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('nederlands', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('中文(简体)', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('한국어', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Türkçe', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('magyar', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('svenska', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Suomi', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('norsk', '2019-04-15', '2019-04-16');
+insert into language (`desc`, `created_at`, `updated_at`) values ('Dansk', '2019-04-15', '2019-04-16');
