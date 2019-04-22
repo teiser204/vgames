acb0f36c3bb59c163517c0ac0c1b9da908489859
Tsakiridis Sotiris <sotiris@serres.gr>
2019-04-22 10:07:21 +0300

    ANDROID: Add progress bar to all image downloads

--- android/app/src/main/java/gr/artibet/vgames/adapters/GalleryAdapter.java
+++ android/app/src/main/java/gr/artibet/vgames/adapters/GalleryAdapter.java
@@ -1,14 +1,21 @@
 package gr.artibet.vgames.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.List;
 
@@ -34,11 +41,13 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
     public static class GalleryViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView imageView;
+        public ProgressBar progressBar;
 
         public GalleryViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
             super(itemView);
 
             imageView = itemView.findViewById(R.id.galleryImage);
+            progressBar = itemView.findViewById(R.id.progressBar);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -75,7 +84,7 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
     }
 
     @Override
-    public void onBindViewHolder(@NonNull GalleryViewHolder galleryViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final GalleryViewHolder galleryViewHolder, int i) {
         String image = mImageList.get(i);
 
         // Load image
@@ -83,6 +92,19 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
                 .load(image)
                 .centerCrop()
                 .placeholder(R.drawable.ic_image_placeholder)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        galleryViewHolder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        galleryViewHolder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(galleryViewHolder.imageView);
 
     }
