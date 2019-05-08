912c5e4481e312f95e2cc28ce2e174a5d3936451
Tsakiridis Sotiris <sotiris@serres.gr>
2019-05-08 12:51:08 +0300

    API: Add Rest API for language, genre, company, platform and feature

--- djangoapi/games/api/views.py
+++ djangoapi/games/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView, api_view
 from rest_framework.response import Response
 from rest_framework import generics
 from django_filters import rest_framework as filters
+from django.http import Http404
 from .filters import LimitFilterBackend
 
 from .serializers import (
@@ -19,42 +20,244 @@ from games.api.filters import GameFilter
 
 
 # --------------------------------------------------------------------
-# Feature view
+# Feature API views
 # --------------------------------------------------------------------
-class FeatureList(generics.ListAPIView):
-    queryset = Feature.objects.all()
-    serializer_class = FeatureSerializer
+class FeatureList(APIView):
+    """
+    List all features, or create a new one.
+    """
+    def get(self, request, format=None):
+        features = Feature.objects.all()
+        serializer = FeatureSerializer(features, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = FeatureSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FeatureDetail(APIView):
+    """
+    Retrieve, update or delete a feature instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Feature.objects.get(pk=pk)
+        except Feature.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        feature = self.get_object(pk)
+        serializer = FeatureSerializer(feature)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        feature = self.get_object(pk)
+        serializer = FeatureSerializer(feature, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        feature = self.get_object(pk)
+        feature.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # --------------------------------------------------------------------
-# Platform view
+# Platform API views
 # --------------------------------------------------------------------
-class PlatformList(generics.ListAPIView):
-    queryset = Platform.objects.all()
-    serializer_class = PlatformSerializer    
+class PlatformList(APIView):
+    """
+    List all platforms, or create a new one.
+    """
+    def get(self, request, format=None):
+        platforms = Platform.objects.all()
+        serializer = PlatformSerializer(platforms, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PlatformDetail(APIView):
+    """
+    Retrieve, update or delete a platform instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Platform.objects.get(pk=pk)
+        except Platform.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        platform = self.get_object(pk)
+        serializer = PlatformSerializer(platform)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        platform = self.get_object(pk)
+        serializer = PlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        platform = self.get_object(pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # --------------------------------------------------------------------
-# Language view
+# Language API views
 # --------------------------------------------------------------------
-class LanguageList(generics.ListAPIView):
-    queryset = Language.objects.all()
-    serializer_class = LanguageSerializer    
+class LanguageList(APIView):
+    """
+    List all languages, or create a new one.
+    """
+    def get(self, request, format=None):
+        languages = Language.objects.all()
+        serializer = LanguageSerializer(languages, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = LanguageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LanguageDetail(APIView):
+    """
+    Retrieve, update or delete a language instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Language.objects.get(pk=pk)
+        except Language.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        language = self.get_object(pk)
+        serializer = LanguageSerializer(language)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        language = self.get_object(pk)
+        serializer = LanguageSerializer(language, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        language = self.get_object(pk)
+        language.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # --------------------------------------------------------------------
-# Genre view
+# Genre API views
 # --------------------------------------------------------------------
-class GenreList(generics.ListAPIView):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
+class GenreList(APIView):
+    """
+    List all genres, or create a new one.
+    """
+    def get(self, request, format=None):
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GenreDetail(APIView):
+    """
+    Retrieve, update or delete a genre instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Genre.objects.get(pk=pk)
+        except Genre.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        genre = self.get_object(pk)
+        genre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # --------------------------------------------------------------------
-# Company view
+# Company API views
 # --------------------------------------------------------------------
-class CompanyList(generics.ListAPIView):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+class CompanyList(APIView):
+    """
+    List all companies, or create a new one.
+    """
+    def get(self, request, format=None):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CompanyDetail(APIView):
+    """
+    Retrieve, update or delete a company instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Company.objects.get(pk=pk)
+        except Company.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        company = self.get_object(pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # --------------------------------------------------------------------
 # Game list
